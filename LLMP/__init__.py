308830cdaa0dbf5fe9e575f95575775372b52fc2
Kenichi Maeda <kenichi.maeda001@chimera.umb.edu>
2024-10-21 19:58:57 -0400

    Update init

--- LLMP/__init__.py
+++ LLMP/__init__.py
@@ -1,6 +1,7 @@
 from .gpimage import GPImage
 from .chatgpt import GPTModel 
 from .llavaq import LLaVAModel
+from .llama import llamaModel
 from .evaluator import Evaluator
 from .evaluator2 import Evaluator2
 from .evaluator4 import Evaluator4
